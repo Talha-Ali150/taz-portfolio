a54362e855f3bf98a7c14c233752ba2876ba6ac9
Talha-Ali150 <syed.talha.ali.150@gmail.com>
2024-05-23 16:52:44 +0500

    addition to projects

--- frontend/src/components/ProjectsSection/ProjectsSection.jsx
+++ frontend/src/components/ProjectsSection/ProjectsSection.jsx
@@ -12,18 +12,18 @@ const ProjectsSection = () => {
       sourceLink: "https://github.com/Talha-Ali150/turo",
     },
     {
-      pic: require("../../assets/pic1.png"),
-      heading: "Turo Frontend",
-      para: "This is a project made to explore antd library and media queries.",
-      demoLink: "https://turo.vercel.app",
-      sourceLink: "https://github.com/Talha-Ali150/turo",
+      pic: require("../../assets/pic2.png"),
+      heading: "E-Store",
+      para: "This is a project made to explore authentication.",
+      demoLink: "https://e-store-frontend-nine.vercel.app",
+      sourceLink: "https://github.com/Talha-Ali150/e-store",
     },
     {
-      pic: require("../../assets/pic1.png"),
-      heading: "Turo Frontend",
-      para: "This is a project made to explore antd library and media queries.",
-      demoLink: "https://turo.vercel.app",
-      sourceLink: "https://github.com/Talha-Ali150/turo",
+      pic: require("../../assets/pic3.png"),
+      heading: "Corporate Prism",
+      para: "This is a project made to practice adding cutsom touch to libraries.",
+      demoLink: "https://corporateprism.com",
+      // sourceLink: "https://github.com/Talha-Ali150/turo",
     },
   ];
   return (
